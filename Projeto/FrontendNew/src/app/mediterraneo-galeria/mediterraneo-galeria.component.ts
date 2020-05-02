d5f6f8fd2d60b4af41a3f96cc0ecb1cb0d619d55
fc51050 <34450544+fr0is@users.noreply.github.com>
2020-05-02 07:16:43 +0100

    Faltava a galeria

--- Projeto/FrontendNew/src/app/mediterraneo-galeria/mediterraneo-galeria.component.ts
+++ Projeto/FrontendNew/src/app/mediterraneo-galeria/mediterraneo-galeria.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-mediterraneo-galeria',
   templateUrl: './mediterraneo-galeria.component.html',
-  styleUrls: ['./mediterraneo-galeria.component.css']
+  styleUrls: ['../../app/dvgaleria/dvgaleria.component.css']
 })
 export class MediterraneoGaleriaComponent implements OnInit {
 
