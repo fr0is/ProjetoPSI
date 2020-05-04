cb6e07a36361dbdfcb0f66088f9dfc745f7e74d3
Alexandre Monteiro <fc51023@alunos.fc.ul.pt>
2020-05-04 01:50:38 +0100

    implementado mas n testado

--- Projeto/backendNew/routes/catalog.js
+++ Projeto/backendNew/routes/catalog.js
@@ -3,11 +3,16 @@ var router = express.Router();
 
 //Controllers
 var quarto_controller = require('../controllers/quartoController');
+var hotel_controller = require('../controllers/hotelController');
 
 /// Quarto Routes ///
 
 //Get lista quartos
-router.get('/quartos', quarto_controller.room_list);
+router.get('/hoteis/:id/quartos', quarto_controller.room_list);
+router.get('/quartos/:id', quarto_controller.quarto_detail);
+
+router.get('/hoteis', hotel_controller.hotel_list);
+router.get('/hoteis/:id', hotel_controller.hotel_detail);
 
 
 module.exports = router;
