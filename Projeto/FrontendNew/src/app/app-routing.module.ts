7a288c9ddf3ae60c23f2bedc6e4655d1d52822cb
fc51050 <34450544+fr0is@users.noreply.github.com>
2020-05-09 16:21:35 +0100

    Update app-routing.module.ts

--- Projeto/FrontendNew/src/app/app-routing.module.ts
+++ Projeto/FrontendNew/src/app/app-routing.module.ts
@@ -45,7 +45,7 @@ const routes: Routes = [
       component: ClienteComponent, 
       children: [
         {
-          path: 'dados',
+          path: '',
           component: ClienteDadosComponent
         },
         {
