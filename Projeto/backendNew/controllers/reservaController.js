b79bf110a231fbdbc3d41384b00461b4a3468b66
Alexandre Monteiro <fc51023@alunos.fc.ul.pt>
2020-05-13 18:41:46 +0100

    A desenvolver o controlador das reservas

--- Projeto/backendNew/controllers/reservaController.js
+++ Projeto/backendNew/controllers/reservaController.js
@@ -13,10 +13,17 @@ exports.reserva_get_email = function(req, res, next) {
         });
 };
 
+exports.reserva_get_quarto = function(req, res, next) {
+    Reserva.find({ 'quarto': req.params.quarto })
+        .exec(function(err, listReservas) {
+            if (err) { return next(err); } // Error in API usage.
+            res.json(listReservas);
+        });
+};
+
 // Handle Utilizador create.
 exports.reserva_create = [
 
-    // Validate fields.
     body('userEmail', 'userEmail must not be empty.').trim(),
     body('quarto', 'quarto must not be empty.').trim(),
     body('metodoDePagamento', 'metodoDePagamento must not be empty.').trim(),
@@ -24,15 +31,11 @@ exports.reserva_create = [
     body('checkIn', 'checkIn must not be empty.').trim(),
     body('checkOut', 'checkOut must not be empty.').trim(),
 
-    // Sanitize fields.
     sanitizeBody('*').escape(),
-    // Process request after validation and sanitization.
     (req, res, next) => {
 
-        // Extract the validation errors from a request.
         const errors = validationResult(req);
 
-        // Create a Utilizador object with escaped and trimmed data.
         var reserva = new Reserva({
             userEmail: req.body.userEmail,
             quarto: req.body.quarto,
@@ -44,11 +47,58 @@ exports.reserva_create = [
         if (!errors.isEmpty()) {
             res.json({ 'message': 'Validation errors' });
         } else {
-            // Data from form is valid. Save Utilizador.
-            utilizador.save(function(err) {
+            reserva.save(function(err) {
                 if (err) { return next(err); }
-                // Successful - redirect to new Utilizador record.
-                res.json({ 'message': 'success' });
+                res.json({ 'message': 'Quarto Reservado' });
+            });
+        }
+    }
+];
+
+exports.reserva_delete = function(req, res, next) {
+    async.parallel({
+        reserva: function (callback) {
+            Reserva.findById(req.body._id).exec(callback);
+        },
+    },function (err, results) {
+        if (err) { return next(err); }
+
+        Reserva.deleteOne({ _id: req.body._id }, function deleteReserva(err) {
+            if (err) { return next(err); }
+            res.json({ 'message': 'Reserva apagada' })
+        });
+    });
+};
+
+exports.reserva_update = [
+    body('userEmail', 'userEmail must not be empty.').trim(),
+    body('quarto', 'quarto must not be empty.').trim(),
+    body('metodoDePagamento', 'metodoDePagamento must not be empty.').trim(),
+    body('morada', 'morada must not be empty.').trim(),
+    body('checkIn', 'checkIn must not be empty.').trim(),
+    body('checkOut', 'checkOut must not be empty.').trim(),
+
+    sanitizeBody('*').escape(),
+    (req, res, next) => {
+
+        const errors = validationResult(req);
+
+        var reserva = new Reserva({
+            _id: req.body._id,
+            userEmail: req.body.userEmail,
+            quarto: req.body.quarto,
+            metodoDePagamento: req.body.metodoDePagamento,
+            morada: req.body.morada,
+            checkIn: req.body.checkIn,
+            checkOut: req.body.checkOut
+        });
+
+        if (!errors.isEmpty()) {
+            res.json({'message': 'Validation errors'});        }
+        else {
+            Reserva.replaceOne({_id: req.body._id}, reserva, function (err, thegenre) {
+                if (err) { return next(err); }
+                res.json({ 'message': 'Reserva atualizada' });
             });
         }
     }
