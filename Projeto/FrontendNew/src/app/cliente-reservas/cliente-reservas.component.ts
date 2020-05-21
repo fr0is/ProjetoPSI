9e2bed1ed0d1b763b562aa26177fc6a692a38e13
fc51050 <34450544+fr0is@users.noreply.github.com>
2020-05-21 16:38:10 +0100

    Update Reserva
    Ainda n esta tudo mas da update

--- Projeto/FrontendNew/src/app/cliente-reservas/cliente-reservas.component.ts
+++ Projeto/FrontendNew/src/app/cliente-reservas/cliente-reservas.component.ts
@@ -14,10 +14,11 @@ import { Quarto } from 'src/quarto';
 @Component({
   selector: 'app-cliente-reservas',
   templateUrl: './cliente-reservas.component.html',
-  styleUrls: ['./cliente-reservas.component.css']
+  styleUrls: ['./cliente-reservas.component.css','editarReserva.css','livro.css']
 })
 export class ClienteReservasComponent implements OnInit {
 
+  alterarDatas=true;
   cartaoMB: CartaoMB ={
     _id: "",
     numero: "",
@@ -54,14 +55,40 @@ export class ClienteReservasComponent implements OnInit {
     checkOut: null,
     preco: 0
   }
+  precoNovaReserva = 0;
+  reservaEdicao: Reserva = {
+    _id: "",
+    userEmail: "",
+    emailReserva: "",
+    indicativoReserva: "",
+    nifReserva: "",
+    nomeReserva: "",
+    telefoneReserva: "",
+    quarto: null,
+    morada: null,
+    metodoDePagamento: null,
+    checkIn: null,
+    checkOut: null,
+    preco: 0
+  }
+  alterarDadosPagamento = false;
+  editar = false;
   quartos: Quarto[] = [];
   reservas: Reserva[] = [];
   cartoes: CartaoMB[] = [];
+  cartoesUser: CartaoMB[] = [];
   down = [];
   precoFiltro: Number = 500;
+  dataFiltro: Date = new Date(1900, 2, 1);
+  checkIn: Date = new Date(2000);
   hotel: Hotel;
   moradas: Morada[] = [];
   filtrar: FormGroup;
+  datasForm: FormGroup;
+  pagamentoForm: FormGroup;
+  checkInNovaReserva= new Date();
+  checkOutNovaReserva= new Date();
+  cartaoId= "";
   link='hoteisPSI/' + sessionStorage.getItem('hotelNome') + '/cliente';
 
   constructor(
@@ -71,22 +98,42 @@ export class ClienteReservasComponent implements OnInit {
     private cartaoMbService: CartaoMBService
   ) { 
     this.filtrar = this.formBuilder.group({
-      precoFiltro: this.formBuilder.control(500),
+      precoFiltro: this.formBuilder.control(this.precoFiltro),
+      dataFiltro: this.formBuilder.control(new Date(1900, 2, 1)),
+    });
+    this.datasForm = this.formBuilder.group({
+      dataCheckIn: this.formBuilder.control(new Date()),
+      dataCheckOut: this.formBuilder.control(new Date()),
     });
   }
 
   ngOnInit(): void {
     this.getReservas();
+    this.getCartoes();
   }
 
-  updatePrecoFiltro(formData){
+  updateFiltro(formData){
     this.precoFiltro = formData.precoFiltro;
+    this.dataFiltro = formData.dataFiltro;
+  }
+
+  dataNice(data){
+    if(new Date(data).getTime() >= new Date(this.dataFiltro).getTime()){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  getCartoes(){
+    this.cartaoMbService.getCartaoEmail(localStorage.getItem('userAtual')).subscribe(listCartao => {
+      this.cartoesUser = listCartao as CartaoMB[];
+    });
   }
 
   getReservas(){
     this.userService.getUserReservas(localStorage.getItem('userAtual')).subscribe(listReservas => {
       this.reservas = listReservas as Reserva[];
-      console.log(this.reservas);
       for(let i = 0; i < this.reservas.length; i++){
         this.hotelService.getHotelQuarto(this.reservas[i].quarto.quarto).subscribe(results => {
           this.quartos[i] = results;
@@ -110,12 +157,73 @@ export class ClienteReservasComponent implements OnInit {
     this.down[i] = !this.down[i];
   }
 
+  onItemChangeCMB(valor){
+    this.cartaoId = valor;
+    console.log(valor);
+  }
+
   delete(reserva){
     alert("delete");
   }
 
   edit(reserva){
-    alert("edit");
+    this.checkInNovaReserva = reserva.checkIn;
+    this.checkInNovaReserva = reserva.checkOut;
+    this.editar = true;
+    this.reservaEdicao = reserva;
   }
 
+  change(datasValue){
+    this.reservaEdicao.checkIn = datasValue.dataCheckIn;
+    this.reservaEdicao.checkOut = datasValue.dataCheckOut;
+    this.calcularPreco(this.reservaEdicao);
+    this.reservaEdicao.preco = this.precoNovaReserva;
+    this.alterarDatas = false;
+    this.alterarDadosPagamento = true;
+  }
+
+  mudaCI(data){
+    this.checkInNovaReserva = data;
+    this.calcularPreco(this.reservaEdicao);
+  }
+
+  mudaCO(data){
+    this.checkOutNovaReserva = data;
+    this.calcularPreco(this.reservaEdicao)
+  }
+
+  calcularPreco(reserva){
+    var dates = [],
+      currentDate = new Date(this.checkInNovaReserva),
+      addDays = function(days) {
+        var date = new Date(this.valueOf());
+        date.setDate(date.getDate() + days);
+        return date;
+      };
+    while (currentDate <=  new Date(this.checkOutNovaReserva)) {
+      dates.push(currentDate);
+      currentDate = addDays.call(currentDate, 1);
+    }
+    var preco = 0;
+    this.hotelService.getHotelQuarto(reserva.quarto.quarto).subscribe(results => {
+      const quarto = results as Quarto;
+      for(let i = 0; i < dates.length-1; i++){
+        if((dates[i].getDate() >= 15 && dates[i].getMonth() >= 1) && (dates[i].getDate() <= 31  && dates[i].getMonth() <= 5) || (dates[i].getDate() >= 30 && dates[i].getMonth() >= 9) && (dates[i].getDate() <= 15  && dates[i].getMonth() <= 12)){
+          preco += quarto.precoBaixa;
+        }else{
+          preco += quarto.precoAlta;
+        }
+      }
+      this.precoNovaReserva = preco;
+    });
+  }
+
+  updateReserva(){
+    this.reservaEdicao.quarto = this.reservaEdicao.quarto._id;
+    this.reservaEdicao.morada = this.reservaEdicao.morada._id;
+    this.reservaEdicao.metodoDePagamento = this.cartaoId;
+    console.log(this.cartaoId);
+    console.log(this.reservaEdicao);
+    this.userService.updateReserva(this.reservaEdicao).subscribe(result => console.log(result.message));
+  }
 }
