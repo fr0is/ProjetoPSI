992af59e19838783536553fa36544b0469718000
fc51050 <34450544+fr0is@users.noreply.github.com>
2020-05-28 22:18:39 +0100

    Update app.js

--- Projeto/backendNew/app.js
+++ Projeto/backendNew/app.js
@@ -15,8 +15,8 @@ var app = express();
 
 app.get('/', (req, res) => res.json({ ping: true }));
 
-//const server = app.listen(3071, () => console.log('Running…'));
-const server = app.listen(3000, () => console.log('Running…'));
+const server = app.listen(3071, () => console.log('Running…'));
+//const server = app.listen(3000, () => console.log('Running…'));
 
 process.on('SIGTERM', shutDown);
 process.on('SIGINT', shutDown);
